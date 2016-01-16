75e74ce715aea68c4a5f0f2c61525f76cbb25060
jennifer spry <mail@jenniferspry.com>
2016-01-16 19:48:16 +0100

    remove console logs

--- src/ImageCreator.java
+++ src/ImageCreator.java
@@ -24,8 +24,6 @@ public class ImageCreator {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
 		Mat result = new Mat(collageOptions.getResultHeight(), collageOptions.getResultWidth(), CvType.CV_8UC3, new Scalar(0, 0, 0));
-		System.out.println("end width: " + collageOptions.getResultWidth());
-		System.out.println("end height: " + collageOptions.getResultHeight());
 		
 		File[] liste = dir.listFiles();
 		
@@ -34,32 +32,23 @@ public class ImageCreator {
 		int imgCount = 1;
 		
 		for (int i = 0; i < liste.length; i++){
-			System.out.println("inserting image: " + liste[i].getName());
-			System.out.println("at: " + x + " / " + y);
 			Mat tmp1 = Imgcodecs.imread(liste[i].getAbsolutePath());
 			// rotate image?
 			if (tmp1.rows() > tmp1.cols()) {
 				tmp1 = rotate90(tmp1);
 			}
 			// resize
-			System.out.println("before:" + tmp1.rows() + " / " + tmp1.cols());
 			float format = (float) tmp1.cols() / (float) tmp1.rows();
-			System.out.println("format chosen: " + collageOptions.getFormat());
-			System.out.println("format: " + format);
+
 			if (format == collageOptions.getFormat()) {
-				System.out.println("SAME");
 				Imgproc.resize(tmp1, tmp1, new Size(collageOptions.getPartWidth(), collageOptions.getPartHeight()));
 			} else if (format < collageOptions.getFormat()) {
-				System.out.println("is smaller");
 				float newWidth = tmp1.cols() * ((float) collageOptions.getPartHeight() / tmp1.rows());
 				Imgproc.resize(tmp1, tmp1, new Size(newWidth, collageOptions.getPartHeight()));
 			} else {
-				System.out.println("is larger");
 				float newHeight = tmp1.rows() * ((float) collageOptions.getPartWidth() / tmp1.cols());
 				Imgproc.resize(tmp1, tmp1, new Size(collageOptions.getPartWidth(), newHeight));
 			}
-			System.out.println("size as:" + collageOptions.getPartWidth() + " / " + collageOptions.getPartHeight());
-			System.out.println("size is:" + tmp1.size().width + " / " + tmp1.size().height);
 			
 			// copy
 			Mat destinationROI = new Mat(result, new Rect (new Point(x, y), tmp1.size()));
@@ -72,7 +61,6 @@ public class ImageCreator {
 			if (y >= collageOptions.getResultHeight() || i == liste.length - 1) {
 				// save image
 				Imgcodecs.imwrite(dir.getAbsolutePath() + collageOptions.getFileName(imgCount), result);
-				System.out.println("saved image to: " + dir.getAbsolutePath() + "\\collage" + imgCount + ".jpg");
 				result = new Mat(collageOptions.getResultHeight(), collageOptions.getResultWidth(), CvType.CV_8UC3, new Scalar(0, 0, 0));
 				x = 0;
 				y = 0;
